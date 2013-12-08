abec90814a8896357a5d39d9df5e0252c3972901
Vitor George <vitor.george@gmail.com>
2013-12-08 14:37:40 -0200

    Simpler front page

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -12,23 +12,9 @@ var mongoose = require('mongoose')
  */
 
 exports.index = function(req, res){
-  var page = (req.param('page') > 0 ? req.param('page') : 1) - 1
-  var perPage = 10
-  var options = {
-    select: 'ibge_id name uf connectionStats.totalConnected connectionStats.totalChecked connectionStats.percentualConnected',
-    sortBy: {'connectionStats.percentualConnected': -1},
-    perPage: perPage,
-    page: page
-  }
-  
-  City.list(options, function(err, cities) {
-    if (err) return res.render('500')
-    City.count().exec(function (err, count) {
-      res.render('home/index', {
-        cities: cities,
-        page: page + 1,
-        pages: Math.ceil(count / perPage)
-      })
+  City.count().exec(function (err, count) {
+    res.render('home/index', {
+      citiesCount: count
     })
   })
 }
