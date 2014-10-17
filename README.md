057003a9e308c8fae5168f46b4a1a90e704eecf0
Vitor George <vitor.george@gmail.com>
2014-10-17 20:11:15 -0300

    read me and upgrading express

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# OSM Roads Monitor
+# City Links
 
-This is a project to monitor road connections data in OpenStreetMap.
+This is a platform to verify the existence of routable connections between cities in OpenStreetMap.
 
 ## How monitoring works
 
 A list of cities is loaded to the platform from a csv file.
 
-For each city, arcs representing back and forward connections to the *n* nearest neighbours are generated.
+For each city, links representing back and forward connections to the *n* nearest neighbours are generated.
 
-Then each arc routability is the checked after defined intervals.
+Then each links routability is the checked after defined intervals.
 
 If a route can't be calculated or is too long, it will be show as broken, allowing mappers to easily spot where road connections have errors or are incomplete. 
 
-## Use
+## Usage
 
 This is still in alpha and do not have public instances.
 
-## Develop
+## Developing
 
 Dependencies:
 
--- config/express.js
+++ config/express.js
@@ -3,71 +3,122 @@
  * Module dependencies.
  */
 
-
 var express = require('express');
 var session = require('express-session');
 var compression = require('compression');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var csrf = require('csurf');
+var multer = require('multer');
+var swig = require('swig');
 
+var mongoStore = require('connect-mongo')(session);
 var flash = require('connect-flash');
+var winston = require('winston');
 var helpers = require('view-helpers');
+var config = require('config');
 var pkg = require('../package.json');
 
-var mongoStore = require('connect-mongo')(session);  
+var env = process.env.NODE_ENV || 'development';
 
-module.exports = function (app, config, passport) {
+/**
+ * Expose
+ */
 
-  app.set('showStackError', true)
+module.exports = function (app, passport) {
 
-    // Compression middleware (should be placed before express.static)
+  // Compression middleware (should be placed before express.static)
   app.use(compression({
     threshold: 512
   }));
 
-  app.use(express.static(config.root + '/public'))
+  // Static files middleware
+  app.use(express.static(config.root + '/public'));
+
+  // Use winston on production
+  var log;
+  if (env !== 'development') {
+    log = {
+      stream: {
+        write: function (message, encoding) {
+          winston.info(message);
+        }
+      }
+    };
+  } else {
+    log = 'dev';
+  }
+
+  // Don't log during tests
+  // Logging middleware
+  if (env !== 'test') app.use(morgan(log));
+
+  // Swig templating engine settings
+  if (env === 'development' || env === 'test') {
+    swig.setDefaults({
+      cache: false
+    });
+  }
 
   // set views path, template engine and default layout
-  app.set('views', config.root + '/app/views')
-  app.set('view engine', 'jade')
-
+  app.engine('html', swig.renderFile);
+  app.set('views', config.root + '/app/views');
+  app.set('view engine', 'html');
 
   // expose package.json to views
   app.use(function (req, res, next) {
-    res.locals.pkg = pkg
-    next()
-  })
+    res.locals.pkg = pkg;
+    res.locals.env = env;
+    next();
+  });
+
+  // bodyParser should be above methodOverride
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(multer());
+  app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  }));
+
+  // CookieParser should be above session
+  app.use(cookieParser());
+  app.use(cookieSession({ secret: 'secret' }));
+  app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: pkg.name,
+    store: new mongoStore({
+      url: config.db,
+      collection : 'sessions'
+    })
+  }));
+
+  // use passport session
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   // connect flash for flash messages - should be declared after sessions
-  app.use(flash())
+  app.use(flash());
 
   // should be declared after session and flash
-  app.use(helpers(pkg.name))
+  app.use(helpers(pkg.name));
 
-  // assume "not found" in the error msgs
-  // is a 404. this is somewhat silly, but
-  // valid, you can do whatever you like, set
-  // properties, use instanceof etc.
-  app.use(function(err, req, res, next){
-    // treat as 404
-    if (err.message
-      && (~err.message.indexOf('not found')
-      || (~err.message.indexOf('Cast to ObjectId failed')))) {
-      return next()
-    }
+  // adds CSRF support
+  if (process.env.NODE_ENV !== 'test') {
+    app.use(csrf());
 
-    // log it
-    // send emails if you want
-    console.error(err.stack)
-
-    // error page
-    res.status(500).render('500', { error: err.stack })
-  })
-
-  // assume 404 since no middleware responded
-  app.use(function(req, res, next){
-    res.status(404).render('404', {
-      url: req.originalUrl,
-      error: 'Not found'
-    })
-  })
-
-}
+    // This could be moved to view-helpers :-)
+    app.use(function (req, res, next) {
+      res.locals.csrf_token = req.csrfToken();
+      next();
+    });
+  }
+};
